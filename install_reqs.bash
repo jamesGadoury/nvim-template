35bf0f05140cea714df3278c4511f1677c57bc38
James Gadoury <gadouryjames@gmail.com>
2025-03-23 16:26:13 +0000

    Update install script & lazy-lock

--- install_reqs.bash
+++ install_reqs.bash
@@ -8,6 +8,14 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
-nvim --headless +"LspInstall clangd" +qall
-nvim --headless +"LspInstall pyright" +qall
-nvim --headless +"MasonInstall bash-language-server" +qall
+
+# Install LSP servers first (they typically take longer)
+echo "Installing clangd (this may take a minute)..."
+nvim --headless -c "LspInstall clangd" -c "sleep 60" -c "qall"
+
+echo "Installing pyright (this may take a minute)..."
+nvim --headless -c "LspInstall pyright" -c "sleep 60" -c "qall"
+
+# Install additional tools with Mason
+echo "Installing bash-language-server, black, and isort..."
+nvim --headless -c "MasonInstall bash-language-server black isort" -c "sleep 60" -c "qall"
